46443d77eac6de10e56152af3c9d1f9124495d73
azizul <azizul@w3engineers.com>
2018-12-04 15:11:40 +0600

    added travis

---
+++ app/src/main/java/com/w3engineers/travistest/MainAdapter.java
@@ -0,0 +1,18 @@
+package com.w3engineers.travistest;
+
+/*
+ *  ****************************************************************************
+ *  * Created by : Md. Azizul Islam on 12/4/2018 at 3:11 PM.
+ *  * Email : azizul@w3engineers.com
+ *  *
+ *  * Purpose:
+ *  *
+ *  * Last edited by : Md. Azizul Islam on 12/4/2018.
+ *  *
+ *  * Last Reviewed by : <Reviewer Name> on <mm/dd/yy>
+ *  ****************************************************************************
+ */
+
+public class MainAdapter {
+    public MainAdapter(){}
+}
